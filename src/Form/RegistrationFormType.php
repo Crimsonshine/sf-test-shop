fe758878de195951c85b5ed6f81c7e6666778617
Extrastage <endlesstimeout@gmail.com>
2022-05-14 05:30:59 +0700

    Added profile & auth

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail:',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'autofocus' => 'autofocus',
+                    'placeholder' => 'Пожалуйста введите ваш почтовый ящик'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Пожалуйста заполните поле'
+                    ]),
+                    new Email([
+                        'message' => 'Пожалуйста введите существующий адрес почты'
+                    ])
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Я соглашаюсь с <a href="#"> политикой конфиденциальности</a> *',
+                'required' => true,
+                'label_html' => true,
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'custom-control-input'
+                ],
+                'label_attr' => [
+                    'class' => 'custom-control-label'
+                ],
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Пожалуйста, поставьте галочку',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Пароль:',
+                'required' => false,
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'autocomplete' => 'new-password'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Пожалуйста введите пароль',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Ваш пароль должен быть не менее {{ limit }} символов',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
